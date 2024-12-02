dfc37044c82187a8ea39a279da9bfc9b27f54289
VSarmiento <89476934+VSarmiento@users.noreply.github.com>
2024-12-02 11:34:40 -0800

    Changed main to call startRecord() instead

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-from utils.intent_ner_integration import process_input
 from SR.microphone_input import startRecord
-#test
 
 def main():    
-    #print("Enter q to quit.")
     startRecord()
-
-    # while True:
-    #     # Get user inputC
-    #     input_string = input("Enter query: ")
-
-    #     if input_string.lower() == "q":
-    #         print("Exiting system.")
-    #         break
-
-    #     # Process the input string through intent classification and NER
-    #     action_data = process_input(input_string)
-    #     print(action_data)
 
 if __name__ == "__main__":
     main()
