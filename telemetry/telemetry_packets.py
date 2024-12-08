ed440fc028a187d1bd6d669cf4b464c6bd65c80e
BrianJ-4 <brian.joshua03@gmail.com>
2024-12-08 11:48:29 -0800

    Implemented structure for CarTelemetryData packet

--- telemetry/telemetry_packets.py
+++ telemetry/telemetry_packets.py
@@ -22,3 +22,36 @@ class PacketHeader:
 
     def get_size(self):
         return struct.calcsize('<HBBBBQfI2B')
+    
+class CarTelemetryData:
+    def __init__(self, data, offset):
+        self.unpack(data, offset)
+
+    def unpack(self, data, offset):
+        telemetry_format = '<HfffBbHBBH4H4B4BH4f4B'
+        size = struct.calcsize(telemetry_format)
+        unpacked_data = struct.unpack_from(telemetry_format, data, offset)
+        (
+            self.speed,
+            self.throttle,
+            self.steer,
+            self.brake,
+            self.clutch,
+            self.gear,
+            self.engine_rpm,
+            self.drs,
+            self.rev_lights_percent,
+            self.rev_lights_bit_value,
+        ) = unpacked_data[:10]
+
+        self.brakes_temperature = list(unpacked_data[10:14])       
+        self.tyres_surface_temperature = list(unpacked_data[14:18])
+        self.tyres_inner_temperature = list(unpacked_data[18:22])  
+        self.engine_temperature = unpacked_data[22]               
+        self.tyres_pressure = list(unpacked_data[23:27])           
+        self.surface_type = list(unpacked_data[27:31])  
+
+        self.size = size
+
+    def get_size(self):
+        return self.size
