802f4fc7e5edee9c5a397bb04c4d58df8f5a9fa5
BrianJ-4 <brian.joshua03@gmail.com>
2024-12-08 12:00:44 -0800

    Added function to receive packet

--- telemetry/telemetry_listener.py
+++ telemetry/telemetry_listener.py
@@ -1,4 +1,5 @@
 import socket
+from telemetry.telemetry_packets import PacketHeader, PacketFactory
 
 class TelemetryListener:
     def __init__(self, host='0.0.0.0', port=20777):
@@ -6,3 +7,9 @@ class TelemetryListener:
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.host, self.port))
+
+    def receive_packet(self):
+        data, _ = self.sock.recvfrom(2048)
+        header = PacketHeader(data)
+        packet = PacketFactory.create_packet(header, data)
+        return packet
