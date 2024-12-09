feb6b9119b310560f8e8f54314133a7280df0c40
BrianJ-4 <brian.joshua03@gmail.com>
2024-12-08 23:52:28 -0800

    Implemented text to speech

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-from threading import Thread
+from queue import Queue
 from telemetry.telemetry_listener import TelemetryListener
 from telemetry.telemetry_data_store import TelemetryDataStore
 from utils.intent_ner_integration import process_input
 from vosk import Model, KaldiRecognizer
-import sounddevice as sd
-import queue
-import sys
-import json
-import time
-import os
 
-voice_input_queue = queue.Queue()
+import json
+import os
+import pyttsx3
+import sounddevice as sd
+import threading
+import time
+
+# Queue for voice input
+voice_input_queue = Queue()
+
+# Function for TTS
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)
+    engine.setProperty('volume', 1.0)
+    engine.say(text)
+    engine.runAndWait()
 
 # Function to handle telemetry listening
 def telemetry_listener(data_store):
     listener = TelemetryListener()
-
     try:
         print("Telemetry listener started")
         while True:
@@ -56,44 +65,58 @@ def getDamageOrTemperatureData(data_store, action_data):
             latest_telemetry = data_store.get_latest_telemetry()
             car_data = latest_telemetry.car_telemetry_data[19]
             brake_temp = car_data.brakes_temperature[position]
-            print(brake_temp)
+            msg = f"That brake's temperature is {brake_temp} degrees."
+            print(msg)
+            threading.Thread(target = speak, args = (msg,)).start()
         elif action_data["INTENT"] == "ask_damage":
             latest_damage = data_store.get_latest_car_damage()
             player_damage_data = latest_damage.car_damage_data[19]
             brake_damage = player_damage_data.brakes_damage[position]
-            print(brake_damage)
+            msg = f"That brake's damage is {brake_damage} percent."
+            print(msg)
+            threading.Thread(target = speak, args = (msg,)).start()
+
     elif part == "tyre" or part == "tire":
         if action_data["INTENT"] == "ask_temperature":
             latest_telemetry = data_store.get_latest_telemetry()
             car_data = latest_telemetry.car_telemetry_data[19]
             tire_temperature = car_data.tyres_surface_temperature[position]
-            print(tire_temperature)
+            msg = f"The tire's temperature is {tire_temperature} degrees."
+            print(msg)
+            threading.Thread(target = speak, args = (msg,)).start()
         elif action_data["INTENT"] == "ask_damage":
             latest_damage = data_store.get_latest_car_damage()
             player_damage_data = latest_damage.car_damage_data[19]
             tire_damage = player_damage_data.tyres_wear[position]
-            print(tire_damage)
+            msg = f"That tire's wear is at {tire_damage} percent."
+            print(msg)
+            threading.Thread(target = speak, args = (msg,)).start()
+
     elif part == "wing":
         if action_data["INTENT"] == "ask_damage":
             latest_damage = data_store.get_latest_car_damage()
             player_damage_data = latest_damage.car_damage_data[19]
             if position == 2:
                 wing_damage = player_damage_data.front_left_wing_damage
+                msg = f"The front left wing damage is {wing_damage} percent."
             elif position == 3:
                 wing_damage = player_damage_data.front_right_wing_damage
+                msg = f"The front right wing damage is {wing_damage} percent."
             elif position == 4:
                 wing_damage = player_damage_data.rear_wing_damage
-            print(wing_damage)
+                msg = f"The rear wing damage is {wing_damage} percent."
+            print(msg)
+            threading.Thread(target = speak, args = (msg,)).start()
 
 def getTelemetryData(data_store, intent):
     telemetry_actions = {
-        "ask_last_lap_time": lambda car_data: print(f"Your last lap was a : {car_data.last_lap_time_in_ms / 1000}"),
-        "ask_current_position": lambda car_data: print(f"You're currently in {car_data.car_position}"),
-        "ask_current_lap": lambda car_data: print(f"It is {car_data.current_lap_num}"),
-        "ask_start_position": lambda car_data: print(f"You started in {car_data.grid_position}"),
-        "ask_remaining_fuel": lambda car_data: print(f"We have {car_data.fuel_remaining_laps} laps of fuel left"),
-        "ask_tire_compound": lambda car_data: print(f"You are using {tyre_compound_mapping.get(car_data.visual_tyre_compound)}"),
-        "ask_tire_age": lambda car_data: print(f"These tires are {car_data.tyres_age_laps} laps old"),
+        "ask_last_lap_time": lambda car_data: f"Your last lap was a {car_data.last_lap_time_in_ms / 1000} seconds.",
+        "ask_current_position": lambda car_data: f"You're currently in P {car_data.car_position}.",
+        "ask_current_lap": lambda car_data: f"It's lap number {car_data.current_lap_num}.",
+        "ask_start_position": lambda car_data: f"You started in grid position {car_data.grid_position}.",
+        "ask_remaining_fuel": lambda car_data: f"We have {car_data.fuel_remaining_laps} laps of fuel left.",
+        "ask_tire_compound": lambda car_data: f"You are using {tyre_compound_mapping.get(car_data.visual_tyre_compound)}.",
+        "ask_tire_age": lambda car_data: f"These tires are {car_data.tyres_age_laps} laps old.",
     }
 
     tyre_compound_mapping = {
@@ -104,16 +127,16 @@ def getTelemetryData(data_store, intent):
         8: "wets"
     }
 
-    if intent in ["ask_last_lap_time", "ask_current_position", "ask_current_lap", "ask_start_position"]:
-        latest_lap_data = data_store.get_latest_lap_data()
-        car_data = latest_lap_data.lap_data[19]
-    elif intent in ["ask_remaining_fuel", "ask_tire_compound", "ask_tire_age"]:
-        latest_status = data_store.get_latest_status()
-        car_data = latest_status.car_status_data[19]
-    else:
-        return
-
-    telemetry_actions[intent](car_data)
+    if intent in telemetry_actions:
+        if "fuel" in intent or "tire" in intent:
+            latest_data = data_store.get_latest_status()
+            car_data = latest_data.car_status_data[19]
+        else:
+            latest_data = data_store.get_latest_lap_data()
+            car_data = latest_data.lap_data[19]
+        msg = telemetry_actions[intent](car_data)
+        print(msg)
+        threading.Thread(target = speak, args = (msg,)).start()
 
 # Function to handle voice input
 def voice_listener(data_store):
@@ -130,7 +153,7 @@ def voice_listener(data_store):
 
     def record_callback(indata, frames, time, status):
         if status:
-            print(status, file=sys.stderr)
+            print(status, file = sys.stderr)
         voice_input_queue.put(bytes(indata))
 
     try:
@@ -141,11 +164,11 @@ def voice_listener(data_store):
                     rec = recognizer.Result()
                     result_dict = json.loads(rec)
                     if result_dict.get("text", ""):
-                        print(f"Recognized Voice Command: {result_dict['text']}") # Need to delete
+                        print(f"Recognized Voice Command: {result_dict['text']}")
                         if result_dict['text'] == "exit":
                             print("Exiting system")
                             break
-                        if result_dict['text'] == "the": #Deals with issue of hearing "the" when quiet
+                        if result_dict['text'] == "the":    #Deals with issue of hearing "the" when quiet
                             continue
                         process_voice_command(data_store, result_dict['text'])
     except Exception as e:
@@ -156,7 +179,7 @@ def main():
     data_store = TelemetryDataStore()
 
     # Start telemetry listener thread
-    telemetry_thread = Thread(target = telemetry_listener, args = (data_store,))
+    telemetry_thread = threading.Thread(target = telemetry_listener, args = (data_store,))
     telemetry_thread.daemon = True
     telemetry_thread.start()
 
